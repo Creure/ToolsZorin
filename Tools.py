6e8d464c501c4e5bee3836135f4245626b2ba039
Creure <54384969+Creure@users.noreply.github.com>
2020-08-03 09:23:43 -0400

    Add files via upload

---
+++ Tools.py
@@ -0,0 +1,13 @@
+import os
+
+sublimeText = ["wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -", 'echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list', "sudo apt-get update", "sudo apt-get install sublime-text"]
+protonVPN = ["sudo apt install -y openvpn dialog python3-pip python3-setuptools", "sudo pip3 install protonvpn-cli" ]
+python3Pip = ["sudo apt-get install python3-pip", "sudo pip3 install protonvpn-cli"]
+
+
+for command in sublimeText:
+	print(os.system(command))
+
+for command in protonVPN:
+	print(os.system(command))
+
